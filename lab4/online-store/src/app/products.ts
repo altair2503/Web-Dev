93bec0f709d22aecb61c66ab555b4c5b8618ea3d
Altair <altair200294@gmail.com>
2023-02-19 15:16:57 +0600

    lab4 first commit

---
+++ lab4/online-store/src/app/products.ts
@@ -0,0 +1,35 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple MacBook Pro 14',
+    price: 994924,
+    description: 'The 14-inch MacBook Pro offers plenty of power for pros in a compact form factor. ' +
+        'It has the powerful M2 Pro chipset configurations.'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id: 3,
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
