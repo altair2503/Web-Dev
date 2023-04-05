e2588c97d0b71c6b3a49f190dcf7b360a74617e0
Altair <altair200294@gmail.com>
2023-04-05 18:47:01 +0600

    third commit with filled database

--- lab8/shop/api/models.py
+++ lab8/shop/api/models.py
@@ -7,10 +7,27 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField(default=1000)
+    description = models.TextField(default="")
+    count = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
     def to_json(self):
         return {
             'id': self.id,
             'name': self.name,
             'price': self.price,
+            'description': self.description,
+            'count': self.count,
+            'is_active': self.is_active,
+        }
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
         }
